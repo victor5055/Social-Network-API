6469caf42b5fed5012cb713a250ddac8caced856
Victor <victor5055@outlook.com>
2023-01-19 15:47:04 -0500

    Updated the user routes.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,15 @@ const router = require('express').Router();
 const {
   getUsers,
   createUser,
+  deleteUser,
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers);
-router.route('/').post(createUser);
+router
+.route('/')
+.get(getUsers)
+.post(createUser)
+.delete(deleteUser);
+
 
 module.exports = router;
