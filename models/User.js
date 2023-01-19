8a1ee694ffdc6e618608df7f4de07bfbd72a8561
Victor <victor5055@outlook.com>
2023-01-19 15:26:54 -0500

    The user model had been updated to include acceptance criteria.

--- models/User.js
+++ models/User.js
@@ -1,15 +1,38 @@
+
+const mongoose = require('mongoose');
+const validator = require('validator');
+
 const userSchema = new mongoose.Schema({
 
     //Add individual properties 
-    username: { type: String, required: true },
-    //Use built-in date method to get current date
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true,
+         //Add validate property to check if email is valid
+    validate: {
+        validator: value => validator.isEmail(value),
+        message: '{VALUE} is not a valid email.'
+      }
+    },
+     //An arrary of 'ObjectId' values that reference a model for Thoughts and Friends
+  thoughts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Thought'
+  }],
+  friends: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Friend'
+  }],
+    //Built-in date method to get the current date
     lastAccessed: { type: Date, default: Date.now },
   });
      
-  //Create an individual document 
-  User.create(
-    {
-      username: "victorpereira",
-    },
-    (err) => (err ? handleError(err) : console.log('New document Created'))
-  );
+// virtual retrieves the length of the user's friends on query
+userSchema.virtual('friendCount').get(function() {
+    return friends.length;
+  });
+  
+  //Using mongoose.model() to compile a model 
+  const User = mongoose.model('User', userSchema);
+
+  //Export User
+  module.exports = User;
