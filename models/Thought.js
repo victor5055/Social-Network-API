1f1b1c2e47b4a758e9292808bb0c77691cf5cc26
Victor <victor5055@outlook.com>
2023-01-23 09:10:49 -0500

    Adding vitual to thought model for date formatting.

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const User = require('./User');
+
 
 //Child document or subdocuments embedded into parent document 
 const reactionSchema = new mongoose.Schema({
   //Add individual properties and their types
   reactionId: { type: mongoose.Types.ObjectId, default: new mongoose.Types.ObjectId() },
   reactionBody: { type: String, required: true, maxlength: 280 },
-  username: { type: String, required: true },
+  username: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true  },
   createdAt: { type: Date, default: Date.now },
 
   //Use built-in date method to get current date
@@ -20,26 +20,28 @@ const thoughtSchema = new mongoose.Schema({
   thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
   //Use built-in date method to get current date
   createdAt: { type: Date, default: Date.now },
-  username: { type: String, required: true },
+  username: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true  },
   reactions: [reactionSchema],
   //Use built-in date method to get current date
   lastAccessed: { type: Date, default: Date.now },
 });
 
-// virtual which retrieves the length of the user's friends on query
-thoughtSchema.pre('save', function(next) {
-    const thoughtId = this._id;
-    User.findOne({ username: this.username }, function (err, user){
-      if (err) return handleError(err);
-      User.findByIdAndUpdate(user._id, { $push: { thoughts: thoughtId } }, next);
-    });
-  });
+//adding the associated user id to the thought
+thoughtSchema.pre('save', async function(next) {
+  const userData = await mongoose.model('User').findById(this.username);
+  userData.thoughts.push(this._id);
+  await userData.save();
+  next();
+});
 
   // virtual retrieves the length of the user's friends on query
 thoughtSchema.virtual('reactionCount').get(function() {
     return reactions.length;
   });
 
+  thoughtSchema.virtual('formatTimestamp').get(function() {
+    return this.createdAt.toLocaleString();
+  })
 //Using mongoose.model() to create a new MongoDB collection
 //and allows you to interact with is using the 'User' model
 const Thought = mongoose.model('Thought', thoughtSchema);
