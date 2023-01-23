c57de5e53be87325fa645a2a9b259592bf24ddc0
Victor <victor5055@outlook.com>
2023-01-23 08:49:45 -0500

    Added create reaction in thought document and finished thought controller.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,19 +1,23 @@
 //import User and thought model
-const { User, Thought } = require('../models'); 
+const { Thought } = require('../models'); 
 
 module.exports = {
   //get all users in thoughts in Thought document for social-NetworkDB collection
   async getThoughts(req, res) {
     try {
         const thoughtData = await Thought.find();
-   
+        thoughtData.formattedTimestamp;
+        res.status(200).json(thoughtData);
+      } catch (err) {
+        res.status(500).json(err);
     }
     },
 //get single thought in Thought document for social-networkDB collection
 async getOneThought(req, res) {
     try {
-      const thoughtData = await Thought.findOne({ _id: req.params.thoughtId });
-      res.status(200).json(thoughtData);
+      const thoughtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId },req.body, { new: true });
+      if (!thoughtData) return res.status(404).json('Thought not found');
+      res.status(200).json(`Thought with id ${req.params.thoughtId} was updated.`);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -37,7 +41,23 @@ async getOneThought(req, res) {
       res.status(500).json(err);
     }
   },
-  //delete a user in User document
+
+   //create a reaction in Thought document for social-networkDB collection
+   async createReaction(req, res) {
+    try{
+      const thoughtData = await Thought.findById(req.params.thoughtId);
+      if (!thoughtData) return res.status(404).json(`Thought not found.`);
+
+      const newReaction = { reactionBody: req.body.reactionBody , username: req.body.username }
+      thoughtData.reactions.push(newReaction);
+
+      await thoughtData.save();
+      res.status(200).json(thoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  //delete a user in User document for social-networkDB collection
   async deleteThought(req, res) {
     try{
       const thoughtId = ObjectId(req.body.id);
