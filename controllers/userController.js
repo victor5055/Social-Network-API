484010ea558f85ba557e1c96cad4446d20d639a6
Victor <victor5055@outlook.com>
2023-01-23 09:04:15 -0500

    Added delete user, create friend and delete a friend in user controller.

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,11 +44,39 @@ async updateUser(req, res) {
     async deleteUser(req, res) {
     try{
        
-        const userData = await User.findOneAndRemove(req.params.userId);
+        const userData = await User.findfindById(req.params.userId);
         if (!userData) return res.status(404).json('User not found');
         res.status(200).json(`User with id ${req.params,userId} was deleted.`);
     } catch (err) {
       res.status(500).json(err);
     }
+  },
+
+ //creates a friend in User document for social-networkDB collection
+ async createFriend(req, res) {
+  try{
+    const userData = await User.findById(req.params.userId);
+    if (!userData) return res.status(404).json(`User not found.`);
+    userData.friends.push(req.params.friendId);
+
+    await userData.save();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
   }
+},
+
+ //deletes a friend in User document for social-networkDB collection
+ async deleteFriend(req, res) {
+  try{
+    const userData = await User.findById(req.params.userId);
+    if (!userData) return res.status(404).json(`User not found.`);
+    userData.friends.pull(req.params.friendId);
+
+    await userData.save();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
 };
